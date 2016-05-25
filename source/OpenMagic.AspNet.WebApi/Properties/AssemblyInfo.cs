d4bb6f454388143a0aeebc67a6eeccb5ec87313f
Tim Murphy <tim@26tp.com>
2016-05-25 14:55:57 +1000

    Add initial projects

---
+++ source/OpenMagic.AspNet.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using Anotar.LibLog;
+using OpenMagic.AspNet.WebApi;
+
+[assembly: AssemblyTitle(Constants.Assembly.Title)]
+[assembly: AssemblyDescription("Extends Microsoft.AspNet.WebApi")]
+[assembly: AssemblyProduct(Constants.Assembly.Product)]
+[assembly: AssemblyCopyright(Constants.Assembly.Copyright)]
+[assembly: AssemblyVersion(Constants.Assembly.Version)]
+[assembly: AssemblyFileVersion(Constants.Assembly.FileVersion)]
+[assembly: LogMinimalMessage]
